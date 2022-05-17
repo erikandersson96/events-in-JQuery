ef7609fea6c816c050a40c7f75714f1179d65c63
erikandersson96 <erik96.andersson@gmail.com>
2022-05-17 17:04:22 +0000

    add comments about jquery effects

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+        $(".stream1").slideToggle(); //Search for "Effects" in jQuery documentation to se all different effects
+        $(".stream1").slideToggle(1000); // set a speed in milliseconds
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
