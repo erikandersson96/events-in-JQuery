161683362108f7f8f2bc1d5f7d071b2c66c71522
erikandersson96 <erik96.andersson@gmail.com>
2022-05-17 17:06:26 +0000

    Add comment about opacity

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $("#stream1_btn").on("click", function() {
         $(".stream1").slideToggle(); //Search for "Effects" in jQuery documentation to se all different effects
         $(".stream1").slideToggle(1000); // set a speed in milliseconds
+        // $(".stream1").fadeTo(1000, 0.5); // here you can also set the speed and opacity 
+
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
